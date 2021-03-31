bed8c2c460f6bc4a27c82fcba4daaf9f06bcd339
Ingo <ikuba@intension.de>
2021-03-31 16:46:50 +0200

    Create Beercloak with Keycloak version 12.0.4

--- beercloak-module/src/main/java/beercloak/BeerManager.java
+++ beercloak-module/src/main/java/beercloak/BeerManager.java
@@ -2,26 +2,27 @@ package beercloak;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
 import org.keycloak.connections.jpa.JpaConnectionProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 
-/**
- * @author <a href="mailto:mitya@cargosoft.ru">Dmitry Telegin</a>
- */
-public class BeerManager {
+public class BeerManager
+{
 
     private final KeycloakSession session;
-    private final EntityManager em;
-    private final RealmModel realm;
+    private final EntityManager   em;
+    private final RealmModel      realm;
 
-    public BeerManager(KeycloakSession session) {
+    public BeerManager(KeycloakSession session)
+    {
         this.session = session;
         this.em = session.getProvider(JpaConnectionProvider.class).getEntityManager();
         realm = session.getContext().getRealm();
     }
 
-    public int removeAllBeer() {
+    public int removeAllBeer()
+    {
         Query query = em.createNamedQuery("removeAllBeers");
         query.setParameter("realmId", realm.getId());
         return query.executeUpdate();
--- beercloak-module/src/main/java/beercloak/providers/BeerEntityProviderFactory.java
+++ beercloak-module/src/main/java/beercloak/providers/BeerEntityProviderFactory.java
@@ -1,6 +1,5 @@
 package beercloak.providers;
 
-import beercloak.BeerManager;
 import org.jboss.logging.Logger;
 import org.keycloak.Config.Scope;
 import org.keycloak.connections.jpa.entityprovider.JpaEntityProvider;
@@ -9,42 +8,49 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.RealmModel;
 
-/**
- * @author <a href="mailto:mitya@cargosoft.ru">Dmitry Telegin</a>
- */
-public class BeerEntityProviderFactory implements JpaEntityProviderFactory {
+import beercloak.BeerManager;
 
-    protected static final String ID = "beer-entity-provider";
-    private static final Logger LOG = Logger.getLogger(BeerEntityProviderFactory.class);
+public class BeerEntityProviderFactory
+    implements JpaEntityProviderFactory
+{
+
+    protected static final String ID  = "beer-entity-provider";
+    private static final Logger   LOG = Logger.getLogger(BeerEntityProviderFactory.class);
 
     @Override
-    public JpaEntityProvider create(KeycloakSession session) {
+    public JpaEntityProvider create(KeycloakSession session)
+    {
         LOG.debug("BeerEntityProviderFactory::create");
         return new BeerEntityProvider();
     }
 
     @Override
-    public String getId() {
+    public String getId()
+    {
         return ID;
     }
 
     @Override
-    public void init(Scope config) {
+    public void init(Scope config)
+    {
     }
 
     @Override
-    public void postInit(KeycloakSessionFactory factory) {
+    public void postInit(KeycloakSessionFactory factory)
+    {
         factory.register((event) -> {
             if (event instanceof RealmModel.RealmRemovedEvent)
-                realmRemoved((RealmModel.RealmRemovedEvent) event);
+                realmRemoved((RealmModel.RealmRemovedEvent)event);
         });
     }
 
     @Override
-    public void close() {
+    public void close()
+    {
     }
 
-    private void realmRemoved(RealmModel.RealmRemovedEvent event) {
+    private void realmRemoved(RealmModel.RealmRemovedEvent event)
+    {
         new BeerManager(event.getKeycloakSession()).removeAllBeer();
     }
 
